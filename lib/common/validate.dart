7ddcfdf68ba49b1433cf2af466b3c20135e2a6d9
Gueorgui Topalski <gueorgui@topalski.es>
2021-01-15 10:22:08 +0100

    feat: add validator for dropdowns

--- lib/common/validate.dart
+++ lib/common/validate.dart
@@ -1,6 +1,13 @@
 class FormValidator {
   static String emptyField(String field) {
-    return field.isEmpty ? 'El campo no puede estar vacio' : null;
+    if (field == null) return 'El campo no puede estar vacio';
+    if (field.isEmpty) return 'El campo no puede estar vacio';
+    return null;
+  }
+
+  static String selectField(dynamic field) {
+    if (field == null) return 'Selecione un valor del desplegable';
+    return null;
   }
 
   static String fieldMustBeNumber(String field) {
