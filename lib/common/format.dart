451d4e670d91971642d6cca137781c7cbb358e2e
Gueorgui Topalski <gueorgui@topalski.es>
2021-01-18 16:58:55 +0100

    feat: determine shift from the hour of the current date feat: spanish locale d/m/y format

--- lib/common/format.dart
+++ lib/common/format.dart
@@ -5,4 +5,14 @@ class DateFormatters {
     } else
       return "${date.day}/${date.month}/${date.year}";
   }
+
+  static String dmy(DateTime date) {
+    return "${date.day}/${date.month}/${date.year}";
+  }
+
+  static int dateToShift(DateTime date) {
+    if (date.hour >= 6 && date.hour < 14) return 1; // Morning shift
+    if (date.hour >= 14 && date.hour < 22) return 2; // Afternoon shift
+    return 3;
+  }
 }
